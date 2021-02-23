0fed8a593cb08d23e7e3feb0cd609150eb11254e
akshaylahudkar <imakshaylahudkar@gmail.com>
2021-02-23 22:12:53 +0530

    Create AddingDigits260A.cpp

---
+++ 260A - Adding Digits/AddingDigits260A.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<string>
+using namespace std;
+ 
+int main()
+{
+	int a,b,n;
+	cin>>a>>b>>n;
+	int flag=0;
+	int ans_a=a;
+	
+		for(int j=0;j<10;j++)
+		{	int temp=ans_a*10 + j;
+		
+			if(temp%b==0)
+			{	
+				ans_a=temp;
+				//cout<<temp<<" temp\n";
+				flag=1;
+				break;
+			}
+			
+			
+			
+		}
+		
+		
+		string ans=to_string(ans_a);
+		
+		if(flag==0)
+			ans="-1";
+		else
+		{
+			
+			
+			
+			for(int i=1;i<n;i++)
+				ans+='0';
+			
+		}
+		
+		
+		
+		cout<<ans<<endl;
+		
+}
